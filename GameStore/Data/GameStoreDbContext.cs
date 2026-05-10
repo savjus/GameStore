2c6984321361d9b66edfadae898207539f4f30fb
savjus <salvposk@gmail.com>
2026-05-10 10:47:42 +0300

    dbcontext games

--- GameStore/Data/GameStoreDbContext.cs
+++ GameStore/Data/GameStoreDbContext.cs
@@ -1,1 +1,29 @@
+using GameStore.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace GameStore.Data;
+
+public class GameStoreDbContext(DbContextOptions<GameStoreDbContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    public DbSet<Platform> Platforms => Set<Platform>();
+
+    public DbSet<GameGenre> GameGenres => Set<GameGenre>();
+
+    public DbSet<GamePlatform> GamePlatforms => Set<GamePlatform>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Game>(entity =>
+        {
+            entity.HasKey(game => game.Id);
+            entity.Property(game => game.Name).IsRequired();
+            entity.Property(game => game.Key).IsRequired();
+            entity.HasIndex(game => game.Key).IsUnique();
+            entity.Property(game => game.Description).HasMaxLength(2000);
+        });
+    }
+}
