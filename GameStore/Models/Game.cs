42132bdf900bc6e50f2c6f7c9c4bae8d1ad86d59
savjus <salvposk@gmail.com>
2026-05-10 10:35:02 +0300

    Updated Model classes

--- GameStore/Models/Game.cs
+++ GameStore/Models/Game.cs
@@ -4,9 +4,13 @@ public class Game
 {
     public Guid Id { get; set; }
 
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
-    public string Key { get; set; }
+    public required string Key { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
+
+    public ICollection<GameGenre> GameGenres { get; set; } = [];
+
+    public ICollection<GamePlatform> GamePlatforms { get; set; } = [];
 }
--- GameStore/Models/Genre.cs
+++ GameStore/Models/Genre.cs
@@ -4,7 +4,11 @@ public class Genre
 {
     public Guid Id { get; set; }
 
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
-    public Guid ParentGenreId { get; set; }
+    public Guid? ParentGenreId { get; set; }
+
+    public ICollection<Genre> SubGenres { get; set; } = [];
+
+    public ICollection<GameGenre> GameGenres { get; set; } = [];
 }
