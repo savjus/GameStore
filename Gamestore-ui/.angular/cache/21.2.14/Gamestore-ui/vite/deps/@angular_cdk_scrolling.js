e186a9823cdf00a620d63940c0bf723e4d6d77b2
savjus <salvposk@gmail.com>
2026-06-08 20:25:00 +0300

    initial  branch

---
+++ Gamestore-ui/.angular/cache/21.2.14/Gamestore-ui/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,51 @@
+import {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-TWVK7VUL.js";
+import "./chunk-GUGIMSVJ.js";
+import {
+  Dir
+} from "./chunk-6NCM5CP7.js";
+import "./chunk-ZTF5Q4KX.js";
+import "./chunk-LFAGZLSV.js";
+import "./chunk-UDLUT2CZ.js";
+import "./chunk-NQLKXZCF.js";
+import "./chunk-XWQQ42E4.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
---
+++ Gamestore-ui/.angular/cache/21.2.14/Gamestore-ui/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,19 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-2AJ62HEE.js";
+import "./chunk-T3OPMRBR.js";
+import "./chunk-ZTF5Q4KX.js";
+import "./chunk-LFAGZLSV.js";
+import "./chunk-UDLUT2CZ.js";
+import "./chunk-NQLKXZCF.js";
+import "./chunk-XWQQ42E4.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
