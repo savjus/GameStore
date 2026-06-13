ec2d905503c50f404caa15a043c8c58fb9fa82bf
savjus <salvposk@gmail.com>
2026-06-13 15:10:25 +0300

    Revert "cleared /bin and /obj files"
    This reverts commit be846cea5c702480cd6cb6671cdbf14f301c0ea8.

---
+++ Gamestore-ui/.angular/cache/21.2.14/Gamestore-ui/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-6NCM5CP7.js";
+import "./chunk-XWQQ42E4.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
